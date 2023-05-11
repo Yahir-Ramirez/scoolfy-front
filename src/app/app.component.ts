1510f1c637cd6114c98396a7c676955c465369f8
Didier Esteban Garcia Olave <didier.garcia@complemento360.com>
2023-05-11 14:30:32 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,116 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Base64 } from '@digitalpersona/core';
+
+
+import { FingerPrintService } from './services/finger-print.service';
+import { AcquisitionStarted, DeviceConnected, DeviceDisconnected, FingerprintReader, SampleFormat, SamplesAcquired } from '@digitalpersona/devices';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fingerPrintComparator_web-front';
+export class AppComponent implements OnInit {
+
+  public reader: FingerprintReader;
+  public infoFingerPrintReader: any;
+  public image: any;
+  public readFile: Object;
+  public objEnvio: any = {};
+  public huellas: Array<any> = [];
+
+
+  constructor(private readonly fingerPrintService: FingerPrintService,
+    private http: HttpClient) {
+
+    this.reader = new FingerprintReader();
+    this.readFile = new FileReader();
+
+  }
+
+
+  ngOnInit(): void {
+    this.reader = new FingerprintReader();
+    this.reader.on('DeviceConnected', this.onDeviceConnected);
+    this.reader.on('DeviceDisconnected', this.onDeviceDisconnected);
+    this.reader.on('AcquisitionStarted', this.onAcquisitionStarted);
+    this.reader.on('SamplesAcquired', this.onSamplesAcquired)
+  }
+
+
+  private onDeviceConnected = (event: DeviceConnected) => {
+    console.log('Hola estoy conectado', event);
+  };
+
+  private onDeviceDisconnected = (event: DeviceDisconnected) => {
+    console.log('Estoy desconectado')
+  };
+
+  private onAcquisitionStarted = (event: AcquisitionStarted) => {
+
+  };
+
+  private onSamplesAcquired = (event: any) => {
+    //console.log(`Huella capturada: ${event.samples[0]}`);
+    if (this.huellas.length < 2) {
+      this.huellas.push(Base64.fromBase64Url(event.samples[0]));
+      this.image = this.parseimage(event.samples[0]);
+    } else {
+      alert('Ojo que ya estan las huellas, envie a verificar')
+    }
+  };
+
+
+  public enrollFingerprint() {
+    this.fingerPrintService.leerHuella().subscribe((huella: any) => {
+      console.log(huella);
+    })
+  }
+
+
+  public detener() {
+    this.reader.stopAcquisition(this.infoFingerPrintReader)
+      .then((response) => {
+        console.log(response);
+      })
+  }
+
+  public capturarHuellaPorfin() {
+    this.reader.startAcquisition(SampleFormat.PngImage, this.infoFingerPrintReader)
+      .then((response) => {
+        console.log(response);
+      })
+  }
+
+  public cuantosDispositivosConectados() {
+    Promise.all([
+      this.reader.enumerateDevices()
+    ]).then((numero) => {
+      console.log(numero);
+      this.infoFingerPrintReader = numero[0][0]
+    });
+  }
+
+  parseimage(image: any) {
+    image = image.replace(/_/g, "/");
+    image = image.replace(/-/g, "+");
+    return image;
+  }
+
+  public sendImg() {
+    this.objEnvio['primerHuella'] = this.huellas[0];
+    this.objEnvio['segundaHuella'] = this.huellas[1];
+    this.http.post(`https://lino.serveo.net/validateFinger`,this.objEnvio).subscribe((r: any) => {
+      console.log(r);
+      if(r){
+        alert('Bienvenido al sistema. Estas autorizado')
+      } else {
+        alert('Falla al ingreso al sistema.No estas autorizado')
+
+      }
+    })
+  }
+
+
 }
---
+++ src/app/services/finger-print.service.ts
@@ -0,0 +1,110 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Base64 } from '@digitalpersona/core';
+
+import {
+  FingerprintReader,
+  SampleFormat,
+} from '@digitalpersona/devices';
+
+import Swal from 'sweetalert2';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FingerPrintService {
+  samples: any[] = [];
+  huella: any;
+  reader: FingerprintReader = new FingerprintReader();
+  sampleObservable= new Subject<any>();
+  samplesObservable= new Subject<any>();
+
+
+  constructor() {}
+
+  private onDeviceConnected = (event: any) => {
+    console.log(event);
+  };
+  private onDeviceDisconnected = (event: any) => {};
+  private onReaderError = (event: any) => {};
+  private onAcquisitionStarted = (event: any) => {};
+  private onSampleAcquired = (event: any) => {
+    Swal.fire({
+      title: 'completado',
+      text: 'Se ha capturado la huella',
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+    });
+    this.sampleObservable.next(Base64.fromBase64Url(event.samples[0]));
+    return (this.huella = Base64.fromBase64Url(event.samples[0]));
+  };
+
+  private onSampleAcquiredMultiple = async (event: any) => {
+    console.log("Huellas capturadas");
+    await Swal.fire({
+      title: `Huella ${this.samples.length + 1} capturada`,
+      text: "Presione aceptar para continuar",
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+    });
+    this.samples.push(Base64.fromBase64Url(event.samples[0]));
+    if (this.samples.length === 3) {
+      this.samplesObservable.next(this.samples);
+      this.samples = [];
+      this.destroyReader();
+    }
+    else{
+    await Swal.fire({
+      title: 'Leyendo huella',
+      text: 'Por favor coloque su dedo en el lector',
+      icon: 'info',
+    });
+  }
+  }
+
+  private async initReaderObservable(cantidad: number = 1) {
+    this.reader.on('DeviceConnected', this.onDeviceConnected);
+    this.reader.on('DeviceDisconnected', this.onDeviceDisconnected);
+    this.reader.on('ErrorOccurred', this.onReaderError);
+    cantidad === 1 ? this.reader.on('SamplesAcquired', this.onSampleAcquired)
+                   : this.reader.on('SamplesAcquired', this.onSampleAcquiredMultiple) ;       
+    this.reader.on('AcquisitionStarted', this.onAcquisitionStarted);
+    await this.reader.startAcquisition(SampleFormat.PngImage).then(() => {
+      Swal.fire({
+        title: 'Leyendo huella',
+        text: 'Por favor coloque su dedo en el lector',
+        icon: 'info',
+      });
+    }).catch(() => {
+      Swal.fire({
+        title: 'Error',
+        text: 'No se pudo iniciar la captura',
+        icon: 'error',
+      });
+    });
+  }
+
+  private async destroyReader() {
+    await this.reader.stopAcquisition();
+    this.reader = this.reader.off();
+  }
+
+  leerHuella(cantidad=1) {
+    console.log('hola mundo');
+    this.initReaderObservable(cantidad);
+    return this.sampleObservable.asObservable();
+   }
+
+   leerHuellas() {
+    this.initReaderObservable(3);
+
+    return this.samplesObservable.asObservable();
+   }
+
+  leerHuellaDestroy(){
+    this.destroyReader();
+  }
+
+  
+}
